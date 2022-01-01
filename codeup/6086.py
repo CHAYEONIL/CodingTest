d66cfb4492296424558e2ecf750cad490a959bc6
CHAYEONIL <jeay123@naver.com>
2022-01-01 18:14:23 +0900

    [2022. 01. 01]

---
+++ codeup/6086.py
@@ -0,0 +1,12 @@
+# 1, 2, 3 ... 을 순서대로 계속 더해 합을 만드는데,
+# 그 합이 입력한 정수보다 작을 동안만 계속 더하는 프로그램을 작성해보자.
+
+n = int(input())
+hap = 0
+num = 1
+
+while(hap < n):
+    hap = hap + num
+    num = num + 1
+
+print(hap)
